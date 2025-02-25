5279f7ef71eac92edf780dfb7987c461b5504675
RounakXics09 <rounak@xics.services>
2025-02-25 09:52:09 +0530

    new (#5)

---
+++ src/components/PremiumPricingCard.js
@@ -0,0 +1,54 @@
+import { Box, Grid2 } from '@mui/material'
+import React, { useEffect } from 'react'
+import Play from '../assets/Crown.svg'
+import Check from '../assets/Check.svg'
+
+const lists =
+    [
+        '30h Fast generations',
+        'Unlimited Relaxed generations',
+        'General commercial terms',
+        'Access to member gallery',
+        'Optional credit top ups',
+        '3 concurrent fast jobs'
+    ]
+
+function PremiumPricingCard() {
+
+    return (
+        <Box className="pricing-card-container">
+            <Grid2 container className="price-cards">
+            <img src={Play} alt="Logo" />
+                    <div className='plan-text'>Premium</div>
+                <Grid2 className='div-center'>
+                    <div className='plan-text-price'>$129</div>
+                    <div className='plan-text-duration'>/month</div>
+                </Grid2>
+                <Grid2>
+                    <div className='plan-text-desc'>
+                        per editor/month
+                        <br />
+                        billed monthly
+                    </div>
+                </Grid2>
+                <Grid2>
+                    {
+                    lists.map((e,i) => {
+                        return(
+                        <div key={i} className='plan-text-detail'>
+                            <img src={Check} alt="Logo1" />
+                            <div>{e}</div>
+                        </div>
+                        )
+                    })
+                    }
+                </Grid2>
+                <Grid2>
+                    Get Started
+                </Grid2>
+            </Grid2>
+        </Box>
+    )
+}
+
+export default PremiumPricingCard
---
+++ src/widgets/details/EnquiryForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EnquiryForm() {
+  return (
+    <div>EnquiryForm</div>
+  )
+}
+
+export default EnquiryForm
