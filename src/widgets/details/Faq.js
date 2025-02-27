d55551db33fe229d7941c0691e395ca44705138c
RounakXics09 <rounak@xics.services>
2025-02-27 09:18:49 +0530

    faq

--- src/widgets/details/Faq.js
+++ src/widgets/details/Faq.js
@@ -3,58 +3,65 @@ import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
+import { Box, Grid2 } from '@mui/material';
 
 export default function Faq() {
     return (
-        <div>
-            <Accordion>
-                <AccordionSummary
-                    // expandIcon={<ArrowDropDownIcon />}
+        <Box className='div-center' sx={{py:5}}>
+            <Grid2 container columnSpacing={5} rowSpacing={2}>
+                <Grid2 size={{ xs: 12,sm:12, md: 12, lg: 12, xl: 12 }} className="faq-headings">Frequently Answer & Questions</Grid2>
+                <Grid2 size={{ xs: 12,sm:12, md: 12, lg: 12, xl: 12 }}>
+                    <Accordion>
+                        <AccordionSummary
+                            // expandIcon={<ArrowDropDownIcon />}
 
-                    aria-controls="panel2-content"
-                    id="panel2-header"
-                >
-                    <Typography component="span">Accordion 2</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion>
-                <AccordionSummary
-                    // expandIcon={<ArrowDropDownIcon />}
+                            aria-controls="panel2-content"
+                            id="panel2-header"
+                        >
+                            <Typography component="span">How to know status of a campaign?</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                Powerful project management tools for your companies of all sizes.
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                </Grid2>
+                <Grid2 size={{ xs: 12,sm:12, md: 12, lg: 12, xl: 12 }}>
+                    <Accordion>
+                        <AccordionSummary
+                            // expandIcon={<ArrowDropDownIcon />}
 
-                    aria-controls="panel2-content"
-                    id="panel2-header"
-                >
-                    <Typography component="span">Accordion 2</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion>
-                <AccordionSummary
-                    // expandIcon={<ArrowDropDownIcon />}
+                            aria-controls="panel2-content"
+                            id="panel2-header"
+                        >
+                            <Typography component="span">How to know status of a campaign?</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                Powerful project management tools for your companies of all sizes.
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                </Grid2>
+                <Grid2 size={{ xs: 12,sm:12, md: 12, lg: 12, xl: 12 }}>
+                    <Accordion>
+                        <AccordionSummary
+                            // expandIcon={<ArrowDropDownIcon />}
 
-                    aria-controls="panel2-content"
-                    id="panel2-header"
-                >
-                    <Typography component="span">Accordion 2</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-        </div>
+                            aria-controls="panel2-content"
+                            id="panel2-header"
+                        >
+                            <Typography component="span">How to know status of a campaign?</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                Powerful project management tools for your companies of all sizes.
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                </Grid2>
+            </Grid2>
+        </Box>
     );
 }
